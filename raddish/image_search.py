108005265a6cce288367ff67b1e117e8ec533632
tundrv0l <tundravol2@gmail.com>
2024-06-22 13:28:09 -0500

    Updates from today, implemented event hooks, added actualyl wait time functionality

--- raddish/image_search.py
+++ raddish/image_search.py
@@ -4,10 +4,14 @@
 # Description: A file containing functions related to image searches
 -----------------'''
 
+
 #---Imports---#
 import pyautogui
 from pyautogui import ImageNotFoundException
 from search_rectangle import SearchRectangle
+import time
+from typing import Callable as function
+
 
 def locateImage(image_path: str, confidence: float = 0.9, region: list[int] = None, search_rectangle: SearchRectangle = None, \
                 on_success : function = None, on_fail : function = None ) -> tuple[int]:
@@ -46,8 +50,18 @@ def locateImage(image_path: str, confidence: float = 0.9, region: list[int] = No
     # Find the image on the screen
     try:
         image_location = pyautogui.locateOnScreen(image_path, confidence=confidence, region=(top, left, width, height))
+
+        # Trigger the on_success event hook if the image is found
+        if image_location != None and callable(on_success):
+            on_success(image_location)
+
+        # Return the image location   
         return image_location
+    
+    # Trigger the on_fail event hook if the image is not found
     except ImageNotFoundException as e:
+        if callable(on_fail):
+            on_fail(e)
         raise e
     
 
@@ -88,13 +102,24 @@ def locateAllImages(image_path: str, confidence: float = 0.9, region: list[int] 
     # Find the image on the screen
     try:
         image_locations = pyautogui.locateAllOnScreen(image_path, confidence=confidence, region=(top, left, width, height))
+
+        # Trigger the on_success event hook if the image is found
+        if image_locations != None and callable(on_success):
+            on_success(image_locations)
+    
+        # Return the image locations
         return image_locations
+    
+    # Trigger the on_fail event hook if the image is not found
     except ImageNotFoundException as e:
+        if callable(on_fail):
+            on_fail(e)
         raise e
     
+
 def clickImage(image_path: str, confidence: float = 0.9, region: list[int] = None, search_rectangle: SearchRectangle = None, timeout = 0):
     '''
-    Click on an image on the screen. Also uses a timeout to sleep after the click.
+    Click on an image on the screen. Will wait for the image to appear if a timeout is provided.
 
     Parameters
     ----------
@@ -113,21 +138,22 @@ def clickImage(image_path: str, confidence: float = 0.9, region: list[int] = Non
 
     timeout : int (Optional)
         The amount of time in seconds to wait for the image to appear on the screen.
-
-    Returns
-    -------
-    tuple[int]
-        A tuple of two integers that represent the x, y coordinates of the image.
     '''
     # Determine the region to search for the image
     top, left, width, height = _determineRegion(region, search_rectangle)
 
     # Find the image on the screen
     try:
-        image_location = pyautogui.locateOnScreen(image_path, confidence=confidence, region=(top, left, width, height))
-        pyautogui.click(image_location, interval=timeout)
+        # Find the image on the screen, wait for a timeout if necessary
+        image_location = waitForImage(image_path, confidence=confidence, region=(top, left, width, height), search_rectangle=search_rectangle, timeout=timeout)
+
+        # Click on the image
+        pyautogui.click(image_location)
+    
+    # Raise an exception if the image is not found
     except ImageNotFoundException as e:
         raise e
+
 
 def clickAllImages(image_path: str, confidence: float = 0.9, region: list[int] = None, search_rectangle: SearchRectangle = None, timeout = 0):
     '''
@@ -150,23 +176,25 @@ def clickAllImages(image_path: str, confidence: float = 0.9, region: list[int] =
 
     timeout : int (Optional)
         The amount of time in seconds to wait for the image to appear on the screen.
-
-    Returns
-    -------
-    list[tuple[int]]
-        A list of tuples of two integers that represent the x, y coordinates of the image.
     '''
     # Determine the region to search for the image
     top, left, width, height = _determineRegion(region, search_rectangle)
 
     # Find the image on the screen
     try:
-        image_locations = pyautogui.locateAllOnScreen(image_path, confidence=confidence, region=(top, left, width, height))
+
+        # Find all instances of the image on the screen, wait for a timeout if necessary
+        image_locations = waitForAllImages(image_path, confidence=confidence, region=(top, left, width, height), search_rectangle=search_rectangle, timeout=timeout)
+
+        # Click on all instances of the image
         for image_location in image_locations:
             pyautogui.click(image_location, interval=timeout)
+
+    # Raise an exception if the image is not found
     except ImageNotFoundException as e:
         raise e
-    
+
+
 def waitForImage(image_path: str, confidence: float = 0.9, region: list[int] = None, search_rectangle: SearchRectangle = None, timeout = 0):
     '''
     Wait for an image to appear on the screen.
@@ -189,14 +217,26 @@ def waitForImage(image_path: str, confidence: float = 0.9, region: list[int] = N
     timeout : int (Optional)
         The amount of time in seconds to wait for the image to appear on the screen.
     '''
-    # Determine the region to search for the image
+     # Determine the region to search for the image
     top, left, width, height = _determineRegion(region, search_rectangle)
 
+    # Record the start time
+    start_time = time.time()
+
     # Find the image on the screen
-    try:
-        pyautogui.locateOnScreen(image_path, confidence=confidence, region=(top, left, width, height), timeout=timeout)
-    except ImageNotFoundException as e:
-        raise e
+    while True:
+        
+        try:
+            pyautogui.locateOnScreen(image_path, confidence=confidence, region=(top, left, width, height))
+            return
+        except ImageNotFoundException as e:
+            pass
+
+        if time.time() - start_time > timeout:
+            raise ImageNotFoundException(f"The image: '{image_path}' was not found within the timeout period.")
+        
+        # Sleep to space out the searches
+        time.sleep(0.1)
     
 def waitForAllImages(image_path: str, confidence: float = 0.9, region: list[int] = None, search_rectangle: SearchRectangle = None, timeout = 0):
     '''
@@ -223,13 +263,24 @@ def waitForAllImages(image_path: str, confidence: float = 0.9, region: list[int]
     # Determine the region to search for the image
     top, left, width, height = _determineRegion(region, search_rectangle)
 
-    # Find the image on the screen
-    try:
-        pyautogui.locateAllOnScreen(image_path, confidence=confidence, region=(top, left, width, height), timeout=timeout)
-    except ImageNotFoundException as e:
-        raise e
+    # Record the start time
+    start_time = time.time()
 
-# TODO: Implement clicker functions, waitforall images, clickImage, etc
+    # Find the images on the screen
+    while True:
+
+        try:
+            pyautogui.locateAllOnScreen(image_path, confidence=confidence, region=(top, left, width, height))
+            return
+        except ImageNotFoundException as e:
+            pass
+        
+        # Raise an exception if the timeout is reached
+        if time.time() - start_time > timeout:
+            raise ImageNotFoundException(f"The image: '{image_path}' was not found within the timeout period.")
+        
+        # Sleep to space out the searches
+        time.sleep(0.1)
 
 
 #---Internal Functions---#
@@ -267,7 +318,7 @@ def _validateRegion(self, region : list[int]):
         Checks if the region provided is valid on the screen.
         '''
 
-        # Check if the SR is off the screen
+        # Check if the region is off the screen
         if region[0] < 0 or region[1] < 0:
             raise ImageNotFoundException("The region is off the screen.")
         elif region[2] == 0 or region[3] == 0:
